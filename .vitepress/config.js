792b42d8c1de19741ebc75106762dd7ef46926f1
nico <805879871@qq.com>
2025-01-14 18:10:08 +0800

    测试

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "OPEN THREE",
-  base:  process.env.VITE_BASE_URL  + '/three-cesium-links',
+  base: './',
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
